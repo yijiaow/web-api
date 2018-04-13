e4b0e966b9219e150a44382870090f511caae97b
Yijiao Wang <ttwyj250@gmail.com>
2018-04-13 09:32:48 -0700

    user can delete note by id. #4

--- index.js
+++ index.js
@@ -17,20 +17,41 @@ app.use(express.static(path.join(__dirname, '/')))
 app.use(bodyParser.json())
 
 app.post('/new-note', (req, res) => {
-  db.collection('notes').insertOne(req.body).catch((err) => {
-    console.log(err)
-  })
+  db
+    .collection('notes')
+    .insertOne(req.body)
+    .catch(() => {
+      res.sendStatus(500)
+    })
   res.sendStatus(201)
 })
 
 app.get('/all-notes', (req, res) => {
-  db.collection('notes').find().toArray().then(notes => res.send(notes)).catch(() => res.sendStatus(500))
+  db
+    .collection('notes')
+    .find()
+    .toArray()
+    .then(notes => res.send(notes))
+    .catch(() => res.sendStatus(500))
 })
 
 app.put('/edit-note/:id', (req, res) => {
   const query = req.body
   delete query._id
-  db.collection('notes').updateOne({ '_id': new ObjectID(req.params.id) }, {$set: query}).then(() => {
-    res.sendStatus(200)
-  }).catch(() => res.sendStatus(500))
+  db
+    .collection('notes')
+    .updateOne({ _id: new ObjectID(req.params.id) }, { $set: query })
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(() => res.sendStatus(500))
+})
+app.delete('/delete-note/:id', (req, res) => {
+  db
+    .collection('notes')
+    .deleteOne({ _id: ObjectID(req.params.id) })
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(() => res.sendStatus(500))
 })
